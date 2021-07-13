33652b2ad3f389311a888ac40d552cb7d8175573
Muhammad Razif Rizqullah <razifrizqullah@gmail.com>
2021-07-13 10:03:25 +0700

    init facade

---
+++ FacadePattern/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace FacadePattern {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Facade Pattern!");
+      Console.WriteLine(" . . . . . . . . . . ");
+    }
+  }
+}
