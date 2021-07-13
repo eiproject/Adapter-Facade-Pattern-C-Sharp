2d08faf71b78181b5f9cd0561e3410971235005b
Muhammad Razif Rizqullah <razifrizqullah@gmail.com>
2021-07-13 10:01:43 +0700

    Add UML

--- AdapterPattern/BusinessLogic/Run.cs
+++ AdapterPattern/BusinessLogic/Run.cs
@@ -7,13 +7,13 @@ namespace AdapterPattern {
     internal Run() {
     }
     internal void Start() {
-      ICar Tesla = new AmericanCar(100.5F, "Tesla");
-      ICar Esemka = new IndonesianCar(125.3F, "Esemka");
+      ICar Tesla = new AmericanCar(120.5F, "Tesla"); // speed in Mph
+      ICar Esemka = new IndonesianCar(125.3F, "Esemka"); // speed in Kmh
 
       Tesla.CheckSpeed();
       Esemka.CheckSpeed();
 
-      CarAdapter AdaptedEsemka = new CarAdapter(Esemka);
+      CarAdapter AdaptedEsemka = new CarAdapter(Esemka); // Turn Kmh to Mph
       MphSpeedDifference SpeedDiff = new MphSpeedDifference(Tesla, AdaptedEsemka);
       SpeedDiff.CheckDifference();
     }
